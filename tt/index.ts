48c04a4b8d376726c704054d26eb286739f083b9
richcao <richcao@tencent.com>
2020-07-03 19:05:19 +0800

    feat

--- tt/index.ts
+++ tt/index.ts
@@ -13,10 +13,6 @@ let inter = new Interpreter({
 
 
 inter.evaluate( `
-let t = [1,2,3].concat([4,5]).slice(1)
-console.info(t)
-for(let i of t.values()){
-    console.info(i)
-}
+
 `);
 console.info('res ', res)
